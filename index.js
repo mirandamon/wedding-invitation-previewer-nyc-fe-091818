562c955907dab70fb5927539cf29e136997998c4
Nathan Miranda <nate845@gmail.com>
2018-11-14 00:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@
 // CSS
 // div's height and width must be higher than the form
 // CSS grid one grid, one row for the header, 
-
+// 2 column grid maybe? left column is the form, right is the invite
+// Flexbox???
+// background-color is black or whatever
+// background-image: 
 
 // JS
